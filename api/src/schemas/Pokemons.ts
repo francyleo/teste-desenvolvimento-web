6fa076087bae0d637cc4f66c79ddf73b277b43b1
Francyleo Matos <programadorleomatos@gmail.com>
2021-05-23 14:52:22 -0300

    feat: create pokemon schema

---
+++ api/src/schemas/Pokemons.ts
@@ -0,0 +1,117 @@
+import mongoose from '../config/database';
+
+const PokemonsSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  pokedex_number: {
+    type: Number,
+    required: true,
+  },
+  img_name: {
+    type: String,
+    required: true,
+  },
+  generation: {
+    type: Number,
+    required: true,
+  },
+  evolution_age: {
+    type: Number,
+    required: true,
+  },
+  evolved: {
+    type: Number,
+    required: true,
+  },
+  family_id: {
+    type: '',
+    required: true,
+  },
+  cross_gen: {
+    type: Number,
+    required: true,
+  },
+  type_one: {
+    type: String,
+    required: true,
+  },
+  type_two: {
+    type: String,
+  },
+  weather_one: {
+    type: String,
+    required: true,
+  },
+  weather_two: {
+    type: String,
+  },
+  stat_total: {
+    type: Number,
+  },
+  atk: {
+    type: Number,
+    required: true,
+  },
+  def: {
+    type: Number,
+    required: true,
+  },
+  sta: {
+    type: Number,
+    required: true,
+  },
+  legendary: {
+    type: Number,
+    required: true,
+  },
+  aquireable: {
+    type: Number,
+    required: true,
+  },
+  spawns: {
+    type: Number,
+    required: true,
+  },
+  regional: {
+    type: Number,
+    required: true,
+  },
+  raidable: {
+    type: Number,
+    required: true,
+  },
+  hatchable: {
+    type: Number,
+    required: true,
+  },
+  shiny: {
+    type: Number,
+    required: true,
+  },
+  nest: {
+    type: Number,
+    required: true,
+  },
+  new: {
+    type: Number,
+    required: true,
+  },
+  not_gettable: {
+    type: Number,
+    required: true,
+  },
+  future_evolve: {
+    type: Number,
+    required: true,
+  },
+});
+
+const Pokemons = mongoose.model('Pokemons', PokemonsSchema);
+
+export default Pokemons;
