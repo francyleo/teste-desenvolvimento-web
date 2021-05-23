02d753f928feebbfb1a0a70432955aeee6f22977
Francyleo Matos <programadorleomatos@gmail.com>
2021-05-23 14:51:49 -0300

    feat: config database

---
+++ api/src/config/database.ts
@@ -0,0 +1,20 @@
+import mongoose, { ConnectOptions } from 'mongoose';
+
+const db_user = process.env.MONGO_USER;
+const db_password = process.env.MONGO_PASSWORD;
+const db_name = process.env.MONGO_DATABASE;
+
+const options: ConnectOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+};
+
+mongoose.connect(
+  `mongodb+srv://${db_user}:${db_password}@cluster0.gpujl.mongodb.net/${db_name}?retryWrites=true&w=majority`,
+  options,
+);
+
+mongoose.Promise = global.Promise;
+
+export default mongoose;
